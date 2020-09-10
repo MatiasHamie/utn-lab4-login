2230198e8101450e97b7b5f2fea9fcc4a6f5e70e
MatiasHamie <mnhamie@gmail.com>
2020-09-10 16:44:30 -0300

    Solucion error firebaseConfig faltante

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDrv-MCHBc3fxcKlgnAM_TGt81M1bynKuw",
+    authDomain: "login-ng-661ca.firebaseapp.com",
+    databaseURL: "https://login-ng-661ca.firebaseio.com",
+    projectId: "login-ng-661ca",
+    storageBucket: "login-ng-661ca.appspot.com",
+    messagingSenderId: "525094202553",
+    appId: "1:525094202553:web:49c321275bebec703d3f68"
+  }
 };
